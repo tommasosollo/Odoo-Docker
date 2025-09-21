c9e8e3dc180fff929d842f4599ee27f7993ab5d9
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2025-09-21 17:49:38 +0200

    yt tut

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    "name": "Hospital Managment System",
+    "author": "Tommaso Sollo",
+    "license": "LGPL-3",
+    "version": "17.0.1.0"
+}
---
+++ custom_addons/om_hospital/models/patient.py
@@ -0,0 +1,4 @@
+from odoo import models, fields
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
