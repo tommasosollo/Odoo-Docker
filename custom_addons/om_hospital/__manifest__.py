460a5aed84d05442819b575eb331f8ff3e75734f
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2025-09-21 22:35:12 +0200

    yt tut

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -2,5 +2,11 @@
     "name": "Hospital Managment System",
     "author": "Tommaso Sollo",
     "license": "LGPL-3",
-    "version": "17.0.1.0"
+    "version": "17.0.1.0",
+
+    "data": [
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/menu.xml"
+    ]
 }
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,4 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _description = "Patient Master"
+
+    name = fields.Char(string="Name", required=True)
+    date_of_birth = fields.Date(string="DOB")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
+
