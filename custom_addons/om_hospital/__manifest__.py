d9339c7d8a9ff8c0cd4956a4b73a9c949745fcba
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2025-09-22 14:30:59 +0200

    yt tut

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -3,10 +3,14 @@
     "author": "Tommaso Sollo",
     "license": "LGPL-3",
     "version": "17.0.1.0",
+    "depends": [
+        "mail"
+    ],
 
     "data": [
         "security/ir.model.access.csv",
         "views/patient_views.xml",
+        "views/patient_readonly_views.xml",
         "views/menu.xml"
     ]
 }
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -4,8 +4,22 @@ from odoo import models, fields, api
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Patient Master"
+    _inherit = 'mail.thread'
 
-    name = fields.Char(string="Name", required=True)
-    date_of_birth = fields.Date(string="DOB")
-    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
+    name = fields.Char(
+        string="Name", 
+        required=True, 
+        tracking=True
+        )
+
+    date_of_birth = fields.Date(
+        string="DOB", 
+        tracking=True
+        )
+
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female")], 
+        string="Gender", 
+        tracking=True
+        )
 
