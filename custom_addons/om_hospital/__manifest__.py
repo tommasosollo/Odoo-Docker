2a2b2ede2186327fdfc8b341cc560fe329851ee9
tommasosollo <114075798+tommasosollo@users.noreply.github.com>
2025-09-29 10:29:24 +0200

    yt tut inheritance

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -5,7 +5,8 @@
     "version": "17.0.1.0",
     "depends": [
         "mail",
-        "product"
+        "product",
+        "account"
     ],
 
     "data": [
@@ -15,6 +16,7 @@
         "views/patient_tag_views.xml",
         "views/patient_readonly_views.xml",
         "views/appointment_views.xml",
+        "views/account_move_views.xml",
         "views/menu.xml",
         "data/sequence.xml",
     ],
--- custom_addons/om_hospital/models/__init__.py
+++ custom_addons/om_hospital/models/__init__.py
@@ -2,3 +2,4 @@ from . import patient
 from . import appointment
 from . import patient_tag
 from . import prescription
+from . import account_move
